1638a24be57960af2aa3c46caccb2cdba524c66d
Sharath Kumar <me@sharathkumar.net>
2014-09-10 08:08:35 -0400

    Cleaned up Project Library dependencies and also added RateThisApp support

--- src/net/sharathkumar/android/apps/knowthyrepresentative/activities/MainActivity.java
+++ src/net/sharathkumar/android/apps/knowthyrepresentative/activities/MainActivity.java
@@ -2,11 +2,14 @@ package net.sharathkumar.android.apps.knowthyrepresentative.activities;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import net.sharathkumar.android.apps.knowthyrepresentative.R;
 import net.sharathkumar.android.apps.knowthyrepresentative.helpers.AsyncHttpRequest;
 import net.sharathkumar.android.apps.knowthyrepresentative.helpers.AsyncZipCodeLocator;
+import net.sharathkumar.android.utils.exceptions.RatingsException;
+import net.sharathkumar.android.utils.ratings.RateThisApp;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -15,8 +18,16 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		AsyncZipCodeLocator automaticZipCodeFinder = new AsyncZipCodeLocator(this, R.id.ZipCodeEntered);
-		automaticZipCodeFinder.processRequest();
+		// Exception should be handled in AsyncZipCodeLocator. 
+		// Adding an extra layer of try catch, just in case something goes wonky!
+		try {
+			AsyncZipCodeLocator automaticZipCodeFinder = new AsyncZipCodeLocator(this, R.id.ZipCodeEntered);
+			automaticZipCodeFinder.processRequest();
+		} catch (Exception err) {
+			Log.e("Exception encountered while Autofinding Zip Code.", err.getLocalizedMessage());
+		}
+		
+		rateThisApp();
 	}
 	
 	public void searchForRepresentativeButtonClicked(View buttonClicked) {
@@ -31,6 +42,14 @@ public class MainActivity extends Activity {
 		else {
 			zipCodeEntryField.setError("Please enter a valid Zip Code.");
 		}
+	}
+	
+	public void rateThisApp() {
+	    try {
+	        RateThisApp.appLaunched(this, "Know Thy Representative", "net.sharathkumar.android.apps.knowthyrepresentative");
+	    } catch (RatingsException err) {
+	        Log.e("MainActivity.rateThisApp()", err.getLocalizedMessage());
+	    }
 	}
 
 }
